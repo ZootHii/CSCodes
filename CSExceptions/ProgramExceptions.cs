5fb723f01c34042f559330d11d43129daa687494
Ahmet <ahmet-yildirim_5858@outlook.com>
2021-01-07 17:33:32 +0300

    Initial commit

--- CSExceptions/ProgramExceptions.cs
+++ CSExceptions/ProgramExceptions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSExceptions
 {
@@ -6,14 +7,67 @@ namespace CSExceptions
     {
         public static void Main(string[] args)
         {
+            //First();
+            
+            /*try
+            {
+                Second();
+            }
+            catch (RecordNotFoundException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }*/
+            
+            // sending to a method a method (gönderilen parametrenin türü method ise) ACTION DELEGATE
+            // makes code clean readable if we have a lot catch blocks
+            /*HandleException(() =>
+            {
+                Second();
+            });*/
+            
+            // the same thing more clear
+            HandleException(Second);
+        }
+        
+        // we put everything in here like a normal method
+        private static void HandleException(Action action)
+        {
+            try
+            {
+                action.Invoke();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+
+        private static void Second()
+        {
+            List<string> list = new List<string> {"Ahmet", "Zotthii", "Yıldırım"};
+
+            if (!list.Contains("Burak"))
+            {
+                throw new RecordNotFoundException(message: "Record Not Found."); // Important
+                //throw new RecordNotFoundException("Record Not Found"); // It can work like in top one (message)
+                
+            }
+            else
+            {
+                Console.WriteLine("Record Found.");
+            }
+        }
+
+        private static void First()
+        {
             // when we try to connect a database we must do exception handling
             // because it can show connection stuff that is important
             try
             {
-                string[] students = new string[3]{"Ahmet", "Zoothii", "Yıldırım"};
+                string[] students = new string[3] {"Ahmet", "Zoothii", "Yıldırım"};
                 students[3] = "Ahmet2"; // throws an error
             }
-            catch (Exception exception) 
+            catch (Exception exception)
             {
                 Console.WriteLine("EXCEPTION!");
                 Console.WriteLine(exception.Message);
---
+++ CSExceptions/RecordNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CSExceptions
+{
+    public class RecordNotFoundException : Exception
+    {
+        public RecordNotFoundException(string message) : base(message) // Important
+        {
+        }
+    }
+}
